97d6cab3f3bf32a2ee8eb9f547b6e6f491287a4b
MataMosca94 <harrisonsef@gmail.com>
2025-02-26 21:59:22 +0000

    Update projetoBanco.py

--- projetoBanco.py
+++ projetoBanco.py
@@ -1,9 +1,9 @@
 menu = """
 
-[d] Depositar
-[s] Sacar
-[e] Extrato
-[q] Sair
+[1] Depositar
+[2] Sacar
+[3] Extrato
+[0] Sair
 
 => """
 
@@ -17,7 +17,7 @@ while True:
 
     opcao = input(menu)
 
-    if opcao == "d":
+    if opcao == "1":
         valor = float(input("Informe o valor do depósito: "))
 
         if valor > 0:
@@ -27,7 +27,7 @@ while True:
         else:
             print("Operação falhou! O valor informado é inválido.")
 
-    elif opcao == "s":
+    elif opcao == "2":
         valor = float(input("Informe o valor do saque: "))
 
         excedeu_saldo = valor > saldo
@@ -37,13 +37,13 @@ while True:
         excedeu_saques = numero_saques >= LIMITE_SAQUES
 
         if excedeu_saldo:
-            print("Operação falhou! Você não tem saldo suficiente.")
+            print("Você não tem saldo suficiente.")
 
         elif excedeu_limite:
-            print("Operação falhou! O valor do saque excede o limite.")
+            print("O valor do saque excede o limite.")
 
         elif excedeu_saques:
-            print("Operação falhou! Número máximo de saques excedido.")
+            print("Número máximo de saques excedido.")
 
         elif valor > 0:
             saldo -= valor
@@ -51,17 +51,16 @@ while True:
             numero_saques += 1
 
         else:
-            print("Operação falhou! O valor informado é inválido.")
+            print("valor informado é inválido.")
 
-    elif opcao == "e":
-        print("\n================ EXTRATO ================")
+    elif opcao == "3":
+        print("\n EXTRATO ")
         print("Não foram realizadas movimentações." if not extrato else extrato)
         print(f"\nSaldo: R$ {saldo:.2f}")
-        print("==========================================")
 
-    elif opcao == "q":d
+    elif opcao == "0":0
     
-        break
+    break
 
-    else:
-        print("Operação inválida, por favor selecione novamente a operação desejada.")
+else:
+        print("selecione novamente a operação desejada.")
