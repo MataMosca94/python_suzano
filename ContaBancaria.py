3c77d8d22049c6ecd63306ba6ae7ed5562dd588f
MataMosca94 <harrisonsef@gmail.com>
2025-03-16 11:24:14 +0000

    Add files via upload

---
+++ ContaBancaria.py
@@ -0,0 +1,36 @@
+
+
+import textwrap
+
+def menu():
+    menu = ***\n 
+    =======menu=====
+    [d]\t depositar
+    [s]\t sacar
+    [e]\t textrato
+    [nc]\t nova Conta 
+    [LC]\t listar contas
+    [nu]\t novo usuario 
+    [q]\t sair 
+    ***
+    return input(textwrap.dedent(menu))
+
+
+
+def depositar(saldo, valor, extrato, /):
+
+def sacar(+, saldo,valor, extrato, limite,numero_saques, limite_saques):
+
+def exibir_extrato(saldo,/,+,extrato):
+
+def criar_usuario(usuarios):
+
+def filtrar_usuario(cpf, usuarios):
+
+def criar_conta(agencia, numero_conta, usuarios):
+
+def listar_conta(contas):
+
+def main():
+
+main()  
